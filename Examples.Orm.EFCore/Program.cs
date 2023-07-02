935ebd6e2f862bd09975915ccb3c8ec76ec85ce3
iTeo <barmin.andrey.nnov@gmail.com>
2023-07-02 13:20:38 +0300

    cleanup

--- Examples.Orm.EFCore/Program.cs
+++ Examples.Orm.EFCore/Program.cs
@@ -1,40 +1,26 @@
 using Examples.Orm.EFCore.Factories;
 using Examples.Orm.EFCore.Infrastructure;
+using Examples.Orm.EFCore.Infrastructure.Configuration;
 using Examples.Orm.EFCore.Models;
 using Microsoft.EntityFrameworkCore;
-using MySql.EntityFrameworkCore.Extensions;
-using Org.BouncyCastle.Asn1.Cms;
-using System.Linq;
 
 Console.OutputEncoding = Encoding.UTF8;
+
+//Проверить работает ли сохранение связанных сущностей без добавления их в контекст ( через добавление в корневую)
+//Добавить аттрибуты связи
+//AppContext.SetSwitch
 
 Host
 .CreateDefaultBuilder(args)
 .ConfigureServices(services => services.AddLogging())
 .ConfigureServices(services => services.AddHostedService<ScopedService>())
-.ConfigureServices(services => services.AddDbContext<AppDataContext>(options =>
-{
-    //options.LogTo(Console.WriteLine);
-    //options.UseInMemoryDatabase("Application");
-    //options.UseSqlite($"Data Source=AppDataContext.db");
-
-    //---------docker run --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=SECRET -d postgres
-    options.UseNpgsql("Host=localhost;Port=5432;Database=postgres;User ID=postgres;Password=SECRET;");
-
-
-
-    //options.UseMySQL("server=localhost;user=root;password=SECRET;database=test;");
-}))
+//Password Stored in    secrets.json    or in envirement    or in vault
+//!!!!Not in appsettings
+//.AddDbMySql()
+.AddDbPostgres()
 .Build()
 .Run()
 ;
-
-//Проверить работает ли сохранение связанных сущностей без добавления их в контекст ( через добавление в корневую)
-
-//Добавить аттрибуты связи
-
-
-
 
 
 internal class ScopedService(IServiceScopeFactory serviceScopeFactory, 
@@ -50,13 +36,7 @@ internal class ScopedService(IServiceScopeFactory serviceScopeFactory,
         logger.Info("Scope");
 
         using AppDataContext context = serviceProvider.GetRequiredService<AppDataContext>();
-
         logger.Info($"context.Database.IsRelational() {context.Database.IsRelational()}");
-        logger.Info($"IsSqlServer {context.Database.IsSqlServer()}");
-        logger.Info($"IsNpgsql {context.Database.IsNpgsql()}");
-        logger.Info($"IsMySql {context.Database.IsMySql()}");
-        logger.Info($"IsSqlite {context.Database.IsSqlite()}");
-
         logger.Warn($".CanConnect() {context.Database.CanConnect()}");
 
 
@@ -109,39 +89,39 @@ internal class ScopedService(IServiceScopeFactory serviceScopeFactory,
         logger.Warn("RunWithQueryAsync");
         var query =
             (from DbObject in context.DbObjects
-            where DbObject.DbObjectId == objectId
-            join area in context.Areas
-            on DbObject.AreaId equals area.DbSubjectAreaId
+             where DbObject.DbObjectId == objectId
+             join area in context.Areas
+             on DbObject.AreaId equals area.DbSubjectAreaId
 
-            join ObjectType in context.DbObjectTypes
-            on DbObject.ObjectTypeId equals ObjectType.DbObjectTypeId
+             join ObjectType in context.DbObjectTypes
+             on DbObject.ObjectTypeId equals ObjectType.DbObjectTypeId
 
-            let Attributes = context.DbObjectAttributes.Where(oAttr => DbObject.DbObjectId == oAttr.DbObjectId).ToList()
-            let Childs = 
-            (
-                from Relation in context.DbRelations
-                where DbObject.DbObjectId == Relation.ParentObjectId
-                join RelationType in context.DbRelationTypes
-                on Relation.DbRelationTypeId equals RelationType.DbRelationTypeId
+             let Attributes = context.DbObjectAttributes.Where(oAttr => DbObject.DbObjectId == oAttr.DbObjectId).ToList()
+             let Childs =
+             (
+                 from Relation in context.DbRelations
+                 where DbObject.DbObjectId == Relation.ParentObjectId
+                 join RelationType in context.DbRelationTypes
+                 on Relation.DbRelationTypeId equals RelationType.DbRelationTypeId
 
-                join ChildrenObject in context.DbObjects
-                on Relation.ChildrenObjectId equals ChildrenObject.DbObjectId
+                 join ChildrenObject in context.DbObjects
+                 on Relation.ChildrenObjectId equals ChildrenObject.DbObjectId
 
-                select new
-                {
-                    Relation,
-                    RelationType,
-                    ChildrenObject
-                }
-            ).ToList()
-            select new
-            {
-                DbObject,
-                area,
-                ObjectType,
-                Attributes,
-                Childs,
-            });
+                 select new
+                 {
+                     Relation,
+                     RelationType,
+                     ChildrenObject
+                 }
+             ).ToList()
+             select new
+             {
+                 DbObject,
+                 area,
+                 ObjectType,
+                 Attributes,
+                 Childs,
+             });
 
         var o = await query.FirstOrDefaultAsync();
 
